86f76e92bd9dd81ecc07d10d620219b3428cc67c
Josh Mathew <jsmathew41@gmail.com>
2023-05-24 08:32:37 -0500

    added api placeholders

--- script.js
+++ script.js
@@ -3,12 +3,15 @@
 let song
 let stopTime = 2000
 let guessTracker = 1
-
+let response
+let answer
 //Functions
 const loadRandSong = async () => {
-    let response = await axios.get('https://api.deezer.com/playlist/3155776842')
+    response = await axios.get('https://api.deezer.com/playlist/3155776842')
     let randInt = Math.floor((Math.random()*99))
     song = new Audio(response.data.tracks.data[randInt].preview)
+    console.log(response.data.tracks.data[randInt].title)
+    answer = response.data.tracks.data[randInt].title
 }
 
 const playSong = async () => {
@@ -25,12 +28,38 @@ const addTime = () => {
     let rect = document.querySelector(`#guess-${guessTracker}`)
     rect.style.backgroundColor = '#FFFFFF'
     guessTracker++
+
+    if(guessTracker>=7) updateFeedbackBox()
+}
+
+const updateFeedbackBox = (num) => {
+    let headphones = document.querySelector(".gg-headset")
+    let stats = document.querySelector(".song-info")
+    switch(num){
+        case 1:
+            headphones.style.opacity = 0.1
+            stats.style.opacity = 1
+            break;
+        case 2:
+            headphones.style.opacity = 1
+            stats.style.opacity = 0
+            break;
+    }
+}
+updateFeedbackBox(1)
+const checkGuess = () => {
+    let guess = document.querySelector('#guess').value
+    let result = document.querySelector('.result')
+    //compare strings, make all lower case and remove special characters
+    if (guess.toLowerCase().replace(/[^a-zA-Z0-9 ]/g, '') == answer.toLowerCase().replace(/[^a-zA-Z0-9 ]/g, '')) result.innerText='Nice!'
+
 }
 
 
 //Buttons
 const playButton = document.querySelector('.play')
 const addButton = document.querySelector('.add-time')
+const submitButton = document.querySelector('#submit')
 
 
 //EventListeners
@@ -38,10 +67,13 @@ playButton.addEventListener('click', async ()=> {
 
     playSong()
 })
+
 addButton.addEventListener('click', addTime)
 
 document.addEventListener('DOMContentLoaded', async ()=> {
     await loadRandSong()
 })
+
+submitButton.addEventListener('click', checkGuess)
 
 
