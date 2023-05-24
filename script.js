97b0517a4abda037711da553a4dbf2835d40b63c
Josh Mathew <jsmathew41@gmail.com>
2023-05-24 12:58:06 -0500

    added animation

--- script.js
+++ script.js
@@ -5,13 +5,28 @@ let stopTime = 2000
 let guessTracker = 1
 let response
 let answer
+let rank
+let songData
 //Functions
 const loadRandSong = async () => {
+    //load song track 
     response = await axios.get('https://api.deezer.com/playlist/3155776842')
-    let randInt = Math.floor((Math.random()*99))
-    song = new Audio(response.data.tracks.data[randInt].preview)
-    console.log(response.data.tracks.data[randInt].title)
-    answer = response.data.tracks.data[randInt].title
+    rank = Math.floor((Math.random()*99))
+    song = new Audio(response.data.tracks.data[rank].preview)
+
+    console.log(response.data.tracks.data[rank].title)
+    answer = response.data.tracks.data[rank].title_short
+    //retrieve necessary data from song
+    let { title_short, link, duration, artist, album, id } = response.data.tracks.data[rank];
+    let artistName = artist.name
+    let albumTitle = album.title
+    let albumArt = album.cover_medium
+
+    let trackResponse = await axios.get(`https://api.deezer.com/track/${id}`)
+    let releaseDate = trackResponse.data.release_date
+    songData = [title_short, link, duration, artistName, albumTitle, albumArt, rank, releaseDate]
+
+    song.addEventListener('timeupdate', updateProgressBar)
 }
 
 const playSong = async () => {
@@ -35,31 +50,65 @@ const addTime = () => {
 const updateFeedbackBox = (num) => {
     let headphones = document.querySelector(".gg-headset")
     let stats = document.querySelector(".song-info")
+    let result = document.querySelector('.result')
+
     switch(num){
         case 1:
             headphones.style.opacity = 0.1
             stats.style.opacity = 1
+            result.innerText='Nice!'
+            updateSongStats()
             break;
         case 2:
             headphones.style.opacity = 1
             stats.style.opacity = 0
+            console.log('hi')
             break;
+
     }
 }
-updateFeedbackBox(1)
-const checkGuess = () => {
-    let guess = document.querySelector('#guess').value
-    let result = document.querySelector('.result')
-    //compare strings, make all lower case and remove special characters
-    if (guess.toLowerCase().replace(/[^a-zA-Z0-9 ]/g, '') == answer.toLowerCase().replace(/[^a-zA-Z0-9 ]/g, '')) result.innerText='Nice!'
+
+const updateSongStats = () => {
+    const albumArt = document.querySelector("#art")
+    const songName = document.querySelector('.song-name')
+    const songArtist = document.querySelector('.song-artist')
+    const songStats = document.querySelectorAll('.stat')
+    
+    albumArt.src = songData[5]
+    songName.innerText = songData[0]
+    songArtist.innerText = songData[3]
+    //album title
+    songStats[0].innerText = `Album Name: ${songData[4]}`
+    songStats[1].innerText = `Release: ${songData[7]}`
+    songStats[2].innerText = `Top 100 Ranking: ${rank}`
+    songStats[3].href = songData[1]
+    songStats[3].innerText = songData[1]
+    songStats[4].innerText = `${Math.trunc(songData[2]/60)}:${songData[2]%60}`
+
 
 }
 
+
+const checkGuess = () => {
+    let guess = document.querySelector('#guess').value
+    //compare strings, make all lower case and remove special characters
+    if (guess.toLowerCase().replace(/[^a-zA-Z0-9 ]/g, '') == answer.toLowerCase().replace(/[^a-zA-Z0-9 ]/g, '')) updateFeedbackBox(1)
+
+}
+
+const updateProgressBar = () => {
+    const musicBar = document.querySelector(".music-player")
+    const progressBar = document.querySelector("#progress-bar")
+    let width = (song.currentTime/14)*musicBar.clientWidth
+    //console.log(song.currentTime, maxDuration, musicBar.clientWidth,width)
+    progressBar.style.width = `${width}px`
+    //console.log(progressBar.clientWidth)
+}
 
 //Buttons
 const playButton = document.querySelector('.play')
 const addButton = document.querySelector('.add-time')
-const submitButton = document.querySelector('#submit')
+//const submitButton = document.querySelector('#submit')
 
 
 //EventListeners
@@ -72,8 +121,17 @@ addButton.addEventListener('click', addTime)
 
 document.addEventListener('DOMContentLoaded', async ()=> {
     await loadRandSong()
+    updateFeedbackBox(2)
 })
 
-submitButton.addEventListener('click', checkGuess)
+//submitButton.addEventListener('click', checkGuess)
+document.onkeydown=function(){
+    if(window.event.keyCode=='13'){
+        checkGuess();
+    }
+}
+
+
+
 
 
